0d05766f278b5e024d3233d3f6852ee0a7a0dc7f
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2022-02-08 15:52:11 +0100

    Create server_requests.py

---
+++ modules/server_requests.py
@@ -0,0 +1,34 @@
+import json
+import requests
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.DEBUG)
+
+
+class Clipboard_conn:
+    def __init__(self, server_url):
+        self.resource_url = server_url + '/api/clipboard'
+
+    def get_clipboard(self):
+        r = requests.get(self.resource_url)
+
+        logging.debug('GET Request against {} with status code {}'.format(self.resource_url, r.status_code))
+        assert r.status_code == 200
+
+        return r.content
+
+    def post_clipboard(self, text):
+        r = requests.post(self.resource_url, json={'text': text})
+
+        logging.debug('POST Request against {} with status code {}'.format(self.resource_url, r.status_code))
+        assert r.status_code == 201
+
+        return r.content
+
+    def delete_clipboard(self):
+        r = requests.delete(self.resource_url)
+
+        logging.debug('DELETE Request against {} with status code {}'.format(self.resource_url, r.status_code))
+        assert r.status_code == 200
+
+        return r.content
